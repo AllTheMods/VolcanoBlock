4a69df1842e0b80f75bc2147b6664323a53b6372
oly2o6 <calumroberts02@hotmail.com>
2023-07-04 00:29:12 +0100

    Disabled magmator and thermo generators

--- kubejs/server_scripts/modpack/lavapower.js
+++ kubejs/server_scripts/modpack/lavapower.js
@@ -2,7 +2,7 @@ ServerEvents.recipes(event => {
 
   event.remove({id: 'ic2:geothermal'})
   event.remove({id: 'thermal:dynamo_magmatic'})
-  //event.remove({id: 'powah:crafting/magmator*'})
-  //event.remove({id: 'powah:crafting/thermo*'})
+  event.remove({id: `/powah:crafting\/magmator/`})
+  event.remove({id: `/powah:crafting\/thermo/`})
   event.remove({id: 'mekanismgenerators:generator/heat'})
 })
