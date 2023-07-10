83ea9d0dfa1fbaa4819ceeae263ad158bf3eb0ab
oly2o6 <calumroberts02@hotmail.com>
2023-07-10 04:01:52 +0100

    Modified the angel ring recipe

--- kubejs/server_scripts/mods/angelring/angelring.js
+++ kubejs/server_scripts/mods/angelring/angelring.js
@@ -1,27 +1,13 @@
 ServerEvents.recipes(event => {
-  event.remove({id: 'angelring:diamond_ring'})
-  event.shaped('angelring:diamond_ring', [
-     'DND',
-     'VEU',
-     'DAD'
-   ], {
-     D: '#forge:storage_blocks/diamond',
-     N: '#forge:storage_blocks/netherite',
-     V: '#forge:storage_blocks/vibranium',
-     U: '#forge:storage_blocks/unobtainium',
-     A: '#forge:storage_blocks/allthemodium',
-     E: '#forge:elytra'
-   })
  event.remove({id: 'angelring:angel_ring'})
  event.shaped('angelring:angel_ring', [
   'CAC',
   'ARA',
-  'DGD'
+  'DAD'
 ], {
-  C: 'ironfurnaces:allthemodium_furnace',
-  A: '#forge:storage_blocks/unobtainium',
+  C: 'minecraft:netherite_ingot',
+  A: '2x alltheores:enderium_ingot',
   R: 'angelring:diamond_ring',
-  D: '#forge:nether_stars',
-  G: '#forge:storage_blocks/unobtainium'
+  D: '#forge:nether_stars'
   })
   })
