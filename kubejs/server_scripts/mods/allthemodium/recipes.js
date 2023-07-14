ab367e6aff089f517f3c1309c7cefb4d000bce8a
oly2o6 <calumroberts02@hotmail.com>
2023-07-14 22:55:50 +0100

    Added modium recipe

--- kubejs/server_scripts/mods/allthemodium/recipes.js
+++ kubejs/server_scripts/mods/allthemodium/recipes.js
@@ -3,4 +3,15 @@ ServerEvents.recipes(event => {
         H: 'allthemodium:piglich_heart'
     }).id('kubejs:shaped_heart_block')
     event.shapeless('9x allthemodium:piglich_heart', 'kubejs:piglich_heart_block').id('kubejs:shapeless_piglich_heart')
+
+    event.shaped('allthemodium:allthemodium_ingot', [
+        ' U ',
+        'UIU',
+        ' U '
+      ], {
+        I: 'minecraft:iron_ingot',
+        U: 'ic2:uumatter',
+        R: 'angelring:diamond_ring',
+        D: '#forge:nether_stars'
+        })
 })
