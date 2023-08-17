02b234711fcee334d40ccc1ab2e3db83070cb530
EnigmaQuip <EnigmaQuip@gmail.com>
2023-08-17 12:29:17 -0600

    more ic2 unifying (dust blocks)

--- kubejs/server_scripts/mods/ic2/recipes.js
+++ kubejs/server_scripts/mods/ic2/recipes.js
@@ -1,3 +1,12 @@
 ServerEvents.recipes(event => {
-    event.remove({type:"ic2:cooking_ic2_serializer", id:/ingot/})
+    event.remove({ type: "ic2:cooking_ic2_serializer", id: /ingot/ })
+
+    let dustBlocks = ["tin", "bronze", "silver", "aluminium"]
+    // iron,gold,copper skipped as they are properly the vanilla blocks
+    dustBlocks.forEach(dust => {
+        event.remove({ id: `ic2:${dust}_dust_block_blast` })
+        event.remove({ id: `ic2:${dust}_dust_block_smelter` })
+        event.smelting(`alltheores:${dust == "aluminium" ? "aluminum" : dust}_block`, `ic2:${dust}_dust_block`).cookingTime(800).id(`kubejs:smelting/ic2/${dust}_dust_block`)
+        event.blasting(`alltheores:${dust == "aluminium" ? "aluminum" : dust}_block`, `ic2:${dust}_dust_block`).cookingTime(400).id(`kubejs:blasting/ic2/${dust}_dust_block`)
+    })
 })
