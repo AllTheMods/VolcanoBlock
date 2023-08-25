85f1093c545c498521e5bc3c4717b4422a303ac8
Draxial <53184989+Draxial@users.noreply.github.com>
2023-08-25 15:22:55 +0100

    Fix issue with compressed spirit -Autocrafting when making compressed sand was giving compressed spirit sand instead of atc sand -Added recipes to convert between spirit soul sand and atc soul sand -Fix quest description to reflect this

--- kubejs/server_scripts/mods/spirit/recipe.js
+++ kubejs/server_scripts/mods/spirit/recipe.js
@@ -1,3 +1,8 @@
 ServerEvents.recipes(event => {
     event.shapeless('9x spirit:soul_steel_ingot', 'spirit:soul_steel_block').id('kubejs:spirit_qol/soul_steel_block_to_ingot')
-})
+
+    //fixes spirit soul sand interferring with autocraft of atc soul sand
+    event.remove({output:'spirit:compressed_soul_sand'})
+    event.shapeless('spirit:compressed_soul_sand', 'allthecompressed:soul_sand_block_1x').id('kubejs:spiritsoulsand_to_atc')
+    event.shapeless('allthecompressed:soul_sand_block_1x', 'spirit:compressed_soul_sand').id('kubejs:atcsoulsand_to_spirit')
+})  
