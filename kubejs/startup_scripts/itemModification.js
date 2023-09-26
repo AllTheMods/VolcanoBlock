7c3aa7f5939d6a2ae0238364e9331c075ce75f55
oly2o6 <calumroberts02@hotmail.com>
2023-09-27 00:06:25 +0100

    Fixed lava cell script

--- kubejs/startup_scripts/itemModification.js
+++ kubejs/startup_scripts/itemModification.js
@@ -1,5 +1,8 @@
 ItemEvents.modification(event => {
-    event.modify(['minecraft:lava_bucket', 'ic2:cell_lava'], , item => {
+    event.modify('minecraft:lava_bucket', , item => {
+        item.setBurnTime(0)
+
+    event.modify('ic2:cell_lava', , item => {
         item.setBurnTime(0)
     })
 })
